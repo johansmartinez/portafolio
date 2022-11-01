84a27d84b44e7e1af2b7469a9d00e55aba3cb0e6
johsnsmartinez <johan.martinez.dev@gmail.com>
2022-10-31 20:49:35 -0500

    arreglando imagenes

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,15 +8,15 @@ function Footer() {
             </div>
             <div className='div-links'>
                 <a target="_blank" rel='noreferrer' className='footer-contact git' href="https://github.com/johansmartinez">
-                    <img className="footer-icon" src="/img/iconmonstr-github-1.svg" alt="Logo github"/>
+                    <img className="footer-icon" src="./img/iconmonstr-github-1.svg" alt="Logo github"/>
                     <span className='span-contact'>@johansmartinez</span>
                 </a>
                 <a target="_blank" rel='noreferrer' className='footer-contact mail' href="mailto:johan.martinez.dev@gmail.com">
-                    <img className="footer-icon" src="/img/iconmonstr-mail-thin.svg" alt="icono correo electrónico"/>
+                    <img className="footer-icon" src="./img/iconmonstr-mail-thin.svg" alt="icono correo electrónico"/>
                     <span className='span-contact'>johan.martinez.dev@gmail.com</span> 
                 </a>
                 <a target="_blank" rel='noreferrer' className='footer-contact linkedin' href="https://www.linkedin.com/in/johan-martinez-b37a8a22a/">
-                    <img className="footer-icon" src="/img/iconmonstr-linkedin-3.svg" alt="logo linkedin"/>
+                    <img className="footer-icon" src="./img/iconmonstr-linkedin-3.svg" alt="logo linkedin"/>
                     <span className='span-contact'>/johan-martinez-b37a8a22a</span>
                 </a>
             </div>
